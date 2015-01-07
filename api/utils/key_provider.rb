ecc1086259af87dd2b466ee99a3576e02dac3957
Grant Pidwell <grantpidwell@infinity-g.com>
2015-01-07 15:06:56 +0000

    Modified KeyProvider

--- api/utils/key_provider.rb
+++ api/utils/key_provider.rb
@@ -4,8 +4,6 @@ require './api/services/config_service'
 class KeyProvider
   def get_admin_key
     config = ConfigurationService.new.get_config
-    key = Base64.strict_encode64("#{config[:admin_username]}:#{config[:admin_password]}").chomp  #.gsub('=', '')
-    puts key
-    key
+    Base64.strict_encode64("#{config[:admin_username]}:#{config[:admin_password]}").chomp  #.gsub('=', '')
   end
 end
