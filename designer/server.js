4ea10dce5d8d3c4f3ef424e5d7099743eb1567d6
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2016-02-24 14:36:11 +0200

    have default route and configurable port

--- designer/server.js
+++ designer/server.js
@@ -4,6 +4,16 @@ var PORT = 8080;
 
 var app = express()
 
-app.use(serveStatic(__dirname + '/www'))
+process.argv.forEach(function (val, index, array) {
+  if (val.indexOf('port=') == 0)
+  	PORT = parseInt(val.split('=')[1]);
+});
+
+app.use(serveStatic(__dirname + '/www'));
+
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/www/index.html');
+});
+
 app.listen(PORT);
 console.log('listening on port ' + PORT);
